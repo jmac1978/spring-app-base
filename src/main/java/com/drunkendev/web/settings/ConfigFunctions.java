7bb8534b55a86b31f5fcedd5908b5dcb3cff6c24
Brett Ryan <brett.ryan@gmail.com>
2016-08-26 16:51:22 +1000

    Will now throw IllegalStateException for missing appConfig.
    - Documentation updates
    - Allow static setting of AppConfig to not need a spring been instance.

--- src/main/java/com/drunkendev/web/settings/ConfigFunctions.java
+++ src/main/java/com/drunkendev/web/settings/ConfigFunctions.java
@@ -25,13 +25,27 @@ import org.springframework.stereotype.Component;
 
 
 /**
+ * Provides functions for taglib functions {@code http://drunkendev.com/taglib/app-config}.
+ *
+ * This bean has been marked as a {@link ApplicationContextAware} {@link Component}
+ * which allows one to use component scanning for package {@link com.drunkendev.web.settings}.
+ *
+ * Component scanning of this class is not strictly required, one may use the static
+ * setter {@link #setAppConfig(AppConfig)} to set the instance.
+ *
+ * <strong>NOTE</strong>: if no {@code config} is set at runtime any calls to {@code get}
+ * values will throw a {@link IllegalStateException}.
+ *
+ * This class does not need to be configured as a bean but must have {@link #setAppConfig(AppConfig)}
+ * called to register the {@link AppConfig} instance.
  *
  * @author  Brett Ryan
+ * @since   1.0
  */
 @Component
 public class ConfigFunctions implements ApplicationContextAware {
 
-    private static AppConfig config;
+    private static AppConfig appConfig;
 
     /**
      * Creates a new {@code ConfigFunctions} instance.
@@ -39,29 +53,109 @@ public class ConfigFunctions implements ApplicationContextAware {
     public ConfigFunctions() {
     }
 
-    public static String getString(String k) {
-        return config == null ? null : config.getString(k);
+    private static void ensureConfig() {
+        if (appConfig == null) {
+            throw new IllegalStateException("appConfig must be set.");
+        }
     }
 
-    public static boolean getBoolean(String k) {
-        return config == null ? false : config.getBoolean(k);
+    /**
+     * Get configured value as a string.
+     *
+     * @param   key
+     *          Key to get value for.
+     * @return  Configured value as a string.
+     * @throws  IllegalStateException
+     *          If {@code appConfig} property has not been set.
+     * @since   1.0
+     */
+    public static String getString(String key) {
+        ensureConfig();
+        return appConfig.getString(key);
     }
 
-    public static int getInt(String k) {
-        return config == null ? 0 : config.getInt(k);
+    /**
+     * Get configured value as a {@code boolean}.
+     *
+     * @param   key
+     *          Key to get value for.
+     * @return  Configured value as a boolean.
+     * @throws  IllegalStateException
+     *          If {@code appConfig} property has not been set.
+     * @since   1.0
+     */
+    public static boolean getBoolean(String key) {
+        ensureConfig();
+        return appConfig.getBoolean(key);
     }
 
-    public static long getLong(String k) {
-        return config == null ? 0L : config.getLong(k);
+    /**
+     * Get configured value as an {@code int}.
+     *
+     * @param   key
+     *          Key to get value for.
+     * @return  Configured value as an integer.
+     * @throws  IllegalStateException
+     *          If {@code appConfig} property has not been set.
+     * @since   1.0
+     */
+    public static int getInt(String key) {
+        ensureConfig();
+        return appConfig.getInt(key);
     }
 
-    public static Object getObject(String k) {
-        return config == null ? null : config.getObject(k);
+    /**
+     * Get configured value as a {@code boolean}.
+     *
+     * @param   key
+     *          Key to get value for.
+     * @return  Configured value as a long.
+     * @throws  IllegalStateException
+     *          If {@code appConfig} property has not been set.
+     * @since   1.0
+     */
+    public static long getLong(String key) {
+        ensureConfig();
+        return appConfig.getLong(key);
     }
 
+    /**
+     * Get configured value as a {@code boolean}.
+     *
+     * @param   key
+     *          Key to get value for.
+     * @return  Configured value as an object.
+     * @throws  IllegalStateException
+     *          If {@code appConfig} property has not been set.
+     * @since   1.0
+     */
+    public static Object getObject(String key) {
+        ensureConfig();
+        return appConfig.getObject(key);
+    }
+
+    /**
+     * Set application context on bean instance.
+     *
+     * @param   applicationContext
+     *          Application context.
+     * @throws  BeansException
+     *          if thrown by application context methods.
+     */
     @Override
-    public void setApplicationContext(ApplicationContext ac) throws BeansException {
-        this.config = ac.getBean(AppConfig.class);
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        ConfigFunctions.appConfig = applicationContext.getBean(AppConfig.class);
+    }
+
+    /**
+     * Set context {@code AppConfig} instance.
+     *
+     * @param   value
+     *          Value for the application configuration.
+     * @since   1.1
+     */
+    public static void setAppConfig(AppConfig value) {
+        appConfig = value;
     }
 
 }

21049b1df4d305f5f420fb60ad798368c20880cb
Brett Ryan <brett.ryan@gmail.com>
2017-03-30 12:07:47 +1100

    Utility classes for timing methods.

---
+++ src/main/java/com/drunkendev/util/TimingResult.java
@@ -0,0 +1,46 @@
+/*
+ * TimingResult.java    Mar 30 2017, 11:57
+ *
+ * Copyright 2017 Drunken Dev.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.drunkendev.util;
+
+import java.time.Duration;
+
+
+/**
+ *
+ * @author  Brett Ryan
+ */
+public class TimingResult {
+
+    private final String name;
+    private final Duration duration;
+
+    public TimingResult(String name, Duration duration) {
+        this.name = name;
+        this.duration = duration;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Duration getDuration() {
+        return duration;
+    }
+
+}
---
+++ src/main/java/com/drunkendev/util/TimingUtils.java
@@ -0,0 +1,48 @@
+/*
+ * TimingUtils.java    Mar 30 2017, 11:27
+ *
+ * Copyright 2017 Drunken Dev.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.drunkendev.util;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.function.Consumer;
+
+
+/**
+ *
+ * @author  Brett Ryan
+ */
+public class TimingUtils {
+
+    public static Duration time(Runnable task) {
+        Instant start = Instant.now();
+        task.run();
+        return Duration.between(start, Instant.now());
+    }
+
+    public static <T> Duration time(Consumer<T> task, T arg) {
+        Instant start = Instant.now();
+        task.accept(arg);
+        return Duration.between(start, Instant.now());
+    }
+
+    public static TimingResult time(String name, Consumer<String> task) {
+        return new TimingResult(name, time(task, name));
+    }
+
+}
